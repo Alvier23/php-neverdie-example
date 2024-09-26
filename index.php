13de63162a2ceaec5b92a3ecd111b53c22fe543e
Alvier23 <falvier@gmail.com>
2024-09-26 22:01:10 +0700

    tes commit

--- index.php
+++ index.php
@@ -6,15 +6,16 @@ $db = $database->connect();
 
 $sql = "SELECT * FROM users WHERE email = :email";
 $sql_2 = "SELECT * FROM kelas WHERE nama_kelas = :nama_kelas";
-$stmt = $db->prepare($sql);
-$stmt_2 = $db->prepare($sql_2);
 
+// Ubah urutan query dan variabel
+$stmt_2 = $db->prepare($sql_2);
+$stmt = $db->prepare($sql);
+
+$stmt_2->execute(['nama_kelas' => 'MIPA 1']);
 $stmt->execute(['email' => 'email@example.com']);
 
-$stmt_2->execute(['nama_kelas' => 'XII 1']);
-$result = $stmt->fetch();
 $result_2 = $stmt_2->fetch();
+$result = $stmt->fetch();
 
-
-print_r($result_2);
 print_r($result);
+print_r($result_2);
